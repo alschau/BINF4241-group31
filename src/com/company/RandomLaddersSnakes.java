a9b1f1bdddb73569c0f14c4a33edb4ac9ad1df7e
plexinio <rachet@gmx.ch>
2019-10-09 11:07:46 +0200

    Random ladders & snakes improved

--- src/com/company/RandomLaddersSnakes.java
+++ src/com/company/RandomLaddersSnakes.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
+public class RandomLaddersSnakes {
 
     public static void main(String[] args) {
 

3a449343401a212dce43f7cb37ae218f8f0369b0
shnippi <janjosef.schnyder@bluewin.ch>
2019-10-16 15:53:30 +0200

    Merge branch 'master' into shnippi
    # Conflicts:
    #	.idea/misc.xml
    #	src/com/Chess/Main.java

--- src/com/Chess/Board.java
+++ src/com/Chess/Board.java
@@ -1,9 +1,45 @@
 package com.Chess;
 
 public class Board {
-    int boardsize = 8;
 
-    int board[][] = new int[boardsize][boardsize];
+    int boardsize;
+
+    public Board(int boardsize){
+
+        this.boardsize = boardsize;
+
+        Schachfigur[][] board = new Schachfigur[boardsize][boardsize];
+
+        for(int i = 0;i<boardsize;i++){
+            for(int j = 0; j<boardsize;j++){
+
+                if((j == 0)&&(i == 0 || i == 7)){
+                    board[i][j] = new Pawn();
+                }
+
+                else if((j == 7)&&(i == 0 || i == 7)){
+                    board[i][j] = new Pawn();
+                }
+
+                else{
+                    board[i][j] = null;
+                }
+
+
+
+            }
+        }
+    }
+
+    public void printboard(){
+        System.out.println(" a  b  c  d  e  f  g  h \n");
+        for(int i=0; i<boardsize;i++){
+            System.out.println("");
+        }
+
+
+    }
+
 
 
 
--- src/com/Chess/Schachfigur.java
+++ src/com/Chess/Schachfigur.java
@@ -3,6 +3,7 @@ package com.Chess;
 public abstract class Schachfigur {
     int alive; //if it still exists or not
     String color;
+    String strrep;
 
 
 }

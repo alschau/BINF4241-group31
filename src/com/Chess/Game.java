e1ba6981a2f8f540d23e0193e054c35aaa534ded
Aline <aline.schaufelberger@uzh.ch>
2019-10-17 12:07:05 +0200

    Game - Check if input Field and Output Field are valid

--- src/com/Chess/Game.java
+++ src/com/Chess/Game.java
@@ -1,5 +1,7 @@
 package com.Chess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
@@ -18,6 +20,9 @@ public class Game {
         this.player2 = p2;
         b.printboard();
         System.out.println("The Game starts!");
+        ArrayList<String> coordinates = new ArrayList<String>( Arrays.asList("a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"));
+        Boolean from_check = true;
+        Boolean to_check = true;
 
 
         while(true){
@@ -28,30 +33,34 @@ public class Game {
             }
 
             Scanner scanner = new Scanner(System.in);
-            System.out.println(player + ", make your move: ");
-            String move = scanner.nextLine();
+            while(from_check){
+                System.out.println(player + ", choose a valid field of a figure ");
+                String from = scanner.nextLine();
+                if(coordinates.contains(from)){
+                    from_check = false;
+                }
 
+                //TODO: Check if one of my figures is on that field
 
-            // Check
+                to_check = true;
+            }
+            while(to_check){
+                System.out.println(player + ", make your move: ");
+                String to = scanner.nextLine();
+                if(coordinates.contains(to)){
+                    to_check = false;
+                }
 
+                //TODO: Check if I can go to that field
+
+                from_check = true;
+            }
 
             b.printboard();
             this.turn++;
+
         }
-
-
-
-
-
-
-
-
     }
-
-    private int dupl(int i){
-        return 2*i;
-    }
-
 
 
     public void printBoard(){

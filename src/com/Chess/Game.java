9363ad62a010111c20cf1e227876c2828b31ca04
Aline <aline.schaufelberger@uzh.ch>
2019-10-17 11:50:26 +0200

    Game Loop

--- src/com/Chess/Game.java
+++ src/com/Chess/Game.java
@@ -1,11 +1,62 @@
 package com.Chess;
 
+import java.util.Scanner;
+
 public class Game {
     Board board;
+    int turn = 1;
+    String color1 = "W";
+    String color2 = "B";
+    String player1;
+    String player2;
+    String player;
 
-    public Game(Board b){
+
+    public Game(Board b, String p1, String p2){
+        this.board = b;
+        this.player1 = p1;
+        this.player2 = p2;
+        b.printboard();
+        System.out.println("The Game starts!");
+
+
+        while(true){
+            if(this.turn%2 == 1){
+                this.player = player1;
+            } else {
+                this.player = player2;
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(player + ", make your move: ");
+            String move = scanner.nextLine();
+
+
+            // Check
+
+
+            b.printboard();
+            this.turn++;
+        }
+
+
+
+
+
+
 
 
     }
+
+    private int dupl(int i){
+        return 2*i;
+    }
+
+
+
+    public void printBoard(){
+        board.printboard();
+    }
+
 
 }
--- src/com/Chess/Main.java
+++ src/com/Chess/Main.java
@@ -1,23 +1,21 @@
 package com.Chess;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        /**System.out.println("hello");
         Scanner scanner = new Scanner(System.in);
         //player names
         System.out.println("Name of player 1: ");
         String name1 = scanner.nextLine();
         System.out.println("Name of player 2: ");
         String name2 = scanner.nextLine();
-        System.out.println(name1);
-
-        String color_name1 = "w";
-        String color_name2 = "b";
-
-        */
-
 
         Board board = new Board();
-        Game game =  new Game(board);
+        Game game =  new Game(board, name1, name2);
+
+
+
 
         board.printboard();
 
@@ -26,7 +24,7 @@ public class Main {
         board.move("h7","d5");
         board.printboard();
 
-        System.out.println(board.getBoard()[0][1].getName());
+
 
 
 

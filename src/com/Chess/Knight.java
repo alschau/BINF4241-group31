d433725a0d568201843a7f62b2717bf37c571e6a
plexinio <rachet@gmx.ch>
2019-10-17 15:44:07 +0200

    Merge branch 'master' of C:\Users\aljos\BINF4241-group31 with conflicts.

--- src/com/Chess/Knight.java
+++ src/com/Chess/Knight.java
@@ -1,11 +1,33 @@
 package com.Chess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Knight implements Schachfigur {
     String color;
     String name;
+    ArrayList<String> names = new ArrayList<String>( Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h") );
 
     public Knight(String c){
         this.color = c;
-        this.name = color +"R";
+        this.name = color +"K";
     }
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Boolean islegal(String a, String b) {
+        int x1 = names.indexOf(String.valueOf(a.charAt(0))); //letter
+        int y1 = Integer.parseInt(String.valueOf(a.charAt(1)))-1; //number
+
+        int x2 = names.indexOf(String.valueOf(b.charAt(0)));
+        int y2 = Integer.parseInt(String.valueOf(b.charAt(1)))-1;
+        return null;
+    }
+
 }

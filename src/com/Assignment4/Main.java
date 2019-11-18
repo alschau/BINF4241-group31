0b46c29015e9cc046c632bc79ec92f4cdf337ba4
Aline <aline.schaufelberger@uzh.ch>
2019-11-18 16:57:41 +0100

    Added Assignment 4

---
+++ src/com/Assignment4/Main.java
@@ -0,0 +1,7 @@
+package com.Assignment4;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/Assignment4/Oven.java
@@ -0,0 +1,12 @@
+package com.Assignment4;
+
+public class Oven {
+
+    public Oven(){
+    }
+
+    public void oven_method(){
+        System.out.println("Oven method works");
+    }
+
+}
